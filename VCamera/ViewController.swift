75f900fbef4cdce812bb88b94989d609df99b319
VassilyChi <arthurvassilychi@gmail.com>
2021-05-28 16:46:53 +0800

    Initial Commit

---
+++ VCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VCamera
+//
+//  Created by VassilyChi on 2021/5/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
